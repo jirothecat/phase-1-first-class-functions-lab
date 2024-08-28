493669f499f9259464fb68cf3de7df33c8cb9c87
Dylan Tin <dylant711@gmail.com>
2024-08-27 23:25:41 -0400

    finished first class lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+    let drivers =['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(0,2);
+}
+
+returnFirstTwoDrivers()
+
+const returnLastTwoDrivers = function(){
+    let drivers =['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(2,4);
+}
+
+returnLastTwoDrivers()
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+    return function(fare){
+        return number * fare;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, driverFunction) {
+    return driverFunction(drivers);
+  };
